44c89d3830b5ea9521e178e771e6dd3b7d5b0ef1
jiy00nn <wldbs204@gmail.com>
2022-01-27 13:56:07 +0900

    feature: Add swagger UI

--- server/src/main/java/com/haenggu/configuration/WebConfig.java
+++ server/src/main/java/com/haenggu/configuration/WebConfig.java
@@ -1,13 +1,37 @@
 package com.haenggu.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 import java.util.List;
 
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        WebMvcConfigurer.super.configureMessageConverters(converters);
+@Configuration
+public class WebConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(false)
+                .select()
+                    .apis(RequestHandlerSelectors.basePackage("com.haenggu.controller"))
+                    .paths(PathSelectors.any())
+                    .paths(PathSelectors.ant("/api/**"))
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("HaengGu")
+                .description("HaengGu API docs")
+                .version("1.0")
+                .build();
     }
 }
