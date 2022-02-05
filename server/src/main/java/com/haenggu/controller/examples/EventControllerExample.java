2b75bc76470eb9ea83d3a3a4a35b78c2fb162096
jiy00nn <wldbs204@gmail.com>
2022-02-06 07:02:24 +0900

    feature: Search event by region

---
+++ server/src/main/java/com/haenggu/controller/examples/EventControllerExample.java
@@ -0,0 +1,12 @@
+package com.haenggu.controller.examples;
+
+public class EventControllerExample {
+    public static final String EVENT_DELETE_SUCCESS    = "{\n" +
+            "    \"code\" : 204\n" +
+            "    \"message\" : \"행사 정보 삭제에 성공하였습니다.\"\n" +
+            "}";
+    public static final String EVENT_NOT_FOUND         = "{\n" +
+            "    \"code\" : 404\n" +
+            "    \"message\" : \"접근할 수 없는 아이디값입니다.\"\n" +
+            "}";
+}
