b3794bf11e0b285660c50707d0f933a5fa4b851e
jiy00nn <wldbs204@gmail.com>
2022-03-15 10:05:51 +0900

    HAEN-40 feature: add getValue Method

--- server/src/main/java/com/haenggu/domain/enums/MbtiType.java
+++ server/src/main/java/com/haenggu/domain/enums/MbtiType.java
@@ -1,20 +1,34 @@
 package com.haenggu.domain.enums;
 
 public enum MbtiType {
-    ISTJ,
-    ISFJ,
-    INFJ,
-    INTJ,
-    ISTP,
-    ISFP,
-    INFP,
-    INTP,
-    ESTP,
-    ESFP,
-    ENFP,
-    ENTP,
-    ESTJ,
-    ESFJ,
-    ENFJ,
-    ENTJ;
+    ISTJ("ISTJ"),
+    ISFJ("ISFJ"),
+    INFJ("INFJ"),
+    INTJ("INTJ"),
+    ISTP("ISTP"),
+    ISFP("ISFP"),
+    INFP("INFP"),
+    INTP("INTP"),
+    ESTP("ESTP"),
+    ESFP("ESFP"),
+    ENFP("ENFP"),
+    ENTP("ENTP"),
+    ESTJ("ESTJ"),
+    ESFJ("ESFJ"),
+    ENFJ("ENFJ"),
+    ENTJ("ENTJ");
+
+    private String value;
+
+    MbtiType(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
