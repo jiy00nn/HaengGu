d6670ef6d29daab17c889b27288c6d7ada391cf5
jiy00nn <wldbs204@gmail.com>
2022-04-25 05:06:43 +0900

    docs: add `EventBasicResponse` schema description

--- server/src/main/java/com/haenggu/http/response/event/EventBasicResponse.java
+++ server/src/main/java/com/haenggu/http/response/event/EventBasicResponse.java
@@ -13,10 +13,15 @@ import java.util.UUID;
 @Schema(description = "행사 기본 정보 응답 DTO")
 @Getter @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class EventBasicResponse extends BasicTimeResponse {
+    private static final String EVENT_ID = "c4b5ba13-a754-41e8-8a5f-6021cb4bbbc1";
+    private static final String EVENT_TITLE = "데이비드 슈리글리 개인전";
+    private static final String EVENT_IMAGE_URI = "https://bit.ly/3EGKodL";
 
-
+    @Schema(description = "행사 아이디", example = EVENT_ID)
     private final UUID eventId;
+    @Schema(description = "행사 제목", example = EVENT_TITLE)
     private final String title;
+    @Schema(description = "행사 대표 이미지 URI", example = EVENT_IMAGE_URI)
     private final String eventImageUri;
 
     public EventBasicResponse(Event event) {
